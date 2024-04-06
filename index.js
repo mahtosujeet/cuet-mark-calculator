cef14f90c9bea2799beb1b708865a77a74182c15
Sujeet Mahto <87647921+MahtoSujeet@users.noreply.github.com>
2024-04-06 08:15:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,331 @@
+ansKey = {
+  "questions": [
+    {
+      "question_id": "6801917431",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917432",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917433",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917434",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917435",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917436",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917437",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917438",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917439",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917440",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917441",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917442",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917443",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917444",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917445",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917446",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917447",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917448",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917449",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917450",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917451",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917452",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917453",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917454",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917455",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917456",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917457",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917458",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917459",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917460",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917461",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917462",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917463",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917464",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917465",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917466",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917467",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917468",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917469",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917470",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917471",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917472",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917473",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917474",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917475",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917476",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917477",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917478",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917479",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917480",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917481",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917482",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917483",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917484",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917485",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917486",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917487",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917488",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917489",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917490",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917491",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917492",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917493",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917494",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917495",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917496",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917497",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917498",
+      "correct_option": 3
+    },
+    {
+      "question_id": "6801917499",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917500",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917501",
+      "correct_option": 2
+    },
+    {
+      "question_id": "6801917502",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917503",
+      "correct_option": 1
+    },
+    {
+      "question_id": "6801917504",
+      "correct_option": 4
+    },
+    {
+      "question_id": "6801917505",
+      "correct_option": 2
+    }
+  ]
+}
+
+q = document.querySelectorAll(".question-pnl")
+totalMark = 0
+correct = 0
+incorrect = 0
+
+q.forEach((quesEle) => {
+  quesId = quesEle.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[1].childNodes[1].outerText.trim()
+  markedAns = quesEle.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[7].childNodes[1].childNodes[0].nodeValue.trim()
+  correctAns = ansKey.questions.find((ele) => ele.question_id === quesId).correct_option
+  if (markedAns == "--") {
+    return
+  }
+  if (markedAns == correctAns) {
+    totalMark += 4
+    correct += 1
+  }
+  else {
+    totalMark -= 1
+    incorrect += 1
+  }
+})
+
+console.log("Total Marks: ", totalMark)
+console.log("Correct: ", correct)
+console.log("Incorrect: ", incorrect)
+console.log("Attemped: ", correct+incorrect)
